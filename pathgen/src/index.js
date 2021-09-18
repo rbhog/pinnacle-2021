de46f8a10cc0711e19dcf73bf2ba441c13c48391
Rithvik <rbhogavilli@gmail.com>
2021-09-18 11:57:11 -0500

    Add mapbox directions API route fetching

---
+++ pathgen/src/index.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+dotenv.config()
+
+console.log(process.env.MAPBOX_ACCESS_TOKEN)
+
+
+var base = "https://api.mapbox.com/directions/v5/mapbox/cycling/";
+
+async function getRoute(waypoints) {
+    points = ""
+
+    waypoints.forEach((waypoint) => {
+        points += `${waypoint[0]},${waypoint[1]};`
+    })
+
+    points = points.substring(0, points.length - 1)
+    console.log(points)
+
+    const req = await axios.get(`${base}${points}?geometries=geojson&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`)
+
+    return {
+        "type": "Feature",
+	    "geometry": {
+    	    "type": "LineString",
+        	"coordinates": req.data.routes[0].geometry.coordinates
+        }
+    }
+
+    // return await axios.get(`${base}${points}?geometries=geojson&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`)//.then((res) => {
+        // console.log(res.data.routes[0].geometry.coordinates)
+
+    //     lineString.geometry.coordinates = res.data.routes[0].geometry.coordinates
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
+
+}
+
+
+getRoute([[-84.518641, 39.134270], [-84.512023, 39.102779]]).then((res) => {
+    console.log(res)
+})
+//     var lineString = {
+//         "type": "Feature",
+// 	    "geometry": {
+//     	    "type": "LineString",
+//         	"coordinates": res.data.routes[0].geometry.coordinates
+//         }
+//     }
+
+
+// })
