9cfb4e50689e6f9350cba340207444dae0d231c6
chiumax <dumblole@gmail.com>
2021-09-18 18:50:34 -0500

    use effect

--- frontend/pinnacle-2021/src/components/Map/index.js
+++ frontend/pinnacle-2021/src/components/Map/index.js
@@ -50,6 +50,155 @@ export default function Map() {
     });
   });
 
+  useEffect(() => {
+    map.addControl(new CompassControl(), 'top-right');
+    map.addControl(new ZoomControl(), 'top-right');
+    map.addControl(
+      new RulerControl({
+        mainColor: '#E21833',
+        secondaryColor: '#414141',
+        units: 'miles',
+        labelFormat: n => `${n.toFixed(2)} miles`,
+      }),
+      'top-right'
+    );
+    map.addControl(new InspectControl(), 'top-right');
+
+    map.on('move', () => {
+      setMapState({
+        lat: map.getCenter().lat.toFixed(4),
+        lng: map.getCenter().lng.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }, [map]);
+
+  useEffect(() => {
+    if (!map) return;
+
+    // Clear distance layer & initiate empty distances[]
+    // map.getSource("distances").setData(featureCollection([]))
+
+    // let distances = []
+    // let speeds = []
+
+    paths.forEach(({ enabled, data }, i) => {
+      let {
+        NAME: name,
+        TIME_START: start,
+        TIME_END: end,
+        TIME_STEP: step,
+      } = data.properties;
+
+      const lineLayerName = 'path_' + name + '_line';
+      const circleLayerName = 'path_' + name + '_circle';
+
+      const endIndex = ~~((time - start) / step);
+      const coords = [...data.geometry.coordinates].slice(0, endIndex + 1);
+
+      const lastPoint = coords[endIndex];
+
+      // Clear circles if not in time range / not enabled
+      // Clear lines if there aren't >= 2 points
+
+      if (!enabled || time < start || time > end)
+        map.getSource(circleLayerName).setData(featureCollection([]));
+
+      if (!enabled || time <= start) {
+        map.getSource(lineLayerName).setData(featureCollection([]));
+        return;
+      }
+
+      // Add line if at least 2 points
+      if (coords.length >= 2) {
+        map.getSource(lineLayerName).setData(lineString(coords));
+        // .setData(bezierSpline(lineString(coords), { resolution: 1000000, sharpness: 0.6 }))
+      }
+
+      // Add circle & perform collision detection if in time range
+      if (lastPoint) {
+        map.getSource(circleLayerName).setData(
+          featureCollection([
+            circle(lastPoint, SAFE_RADIUS, {
+              steps: 500,
+              units: 'kilometers',
+            }),
+          ])
+        );
+      }
+    });
+
+    // setPathSpeeds(speeds)
+  }, [map, time, paths]);
+
+  useEffect(() => {
+    if (!map) return;
+
+    paths.forEach(({ enabled, data }) => {
+      const lineLayerName = 'path_' + data.properties.NAME + '_line';
+      const circleLayerName = 'path_' + data.properties.NAME + '_circle';
+      const lineLayer = map.getSource(lineLayerName);
+
+      // If line layer exists already, show / hide
+      if (lineLayer) {
+        map.setLayoutProperty(
+          lineLayerName,
+          'visibility',
+          enabled ? 'visible' : 'none'
+        );
+
+        return;
+      }
+
+      map.on('mouseenter');
+
+      map.on('click', lineLayerName, e => handleClick(map, e.lngLat, data));
+      map.on('click', circleLayerName, e => handleClick(map, e.lngLat, data));
+
+      map.on('mouseenter', lineLayerName, () => handleMouse(map, 'pointer'));
+      map.on('mouseenter', circleLayerName, () => handleMouse(map, 'pointer'));
+
+      map.on('mouseleave', lineLayerName, () => handleMouse(map, ''));
+      map.on('mouseleave', circleLayerName, () => handleMouse(map, ''));
+
+      // Add initial sources + layers
+      // Initial source has empty featureCollection, since it'll be updated later
+      map
+        .addSource(lineLayerName, {
+          type: 'geojson',
+          data: featureCollection([]),
+        })
+        .addSource(circleLayerName, {
+          type: 'geojson',
+          data: featureCollection([]),
+        })
+        .addLayer({
+          id: lineLayerName,
+          type: 'line',
+          source: lineLayerName,
+          paint: {
+            'line-color': data.properties.COLOR,
+            'line-opacity': 0.4,
+            'line-width': 2,
+          },
+          layout: {
+            'line-cap': 'round',
+            'line-join': 'bevel',
+          },
+        })
+        .addLayer({
+          id: circleLayerName,
+          type: 'fill',
+          source: circleLayerName,
+          paint: {
+            'fill-color': data.properties.COLOR,
+            'fill-opacity': 0.2,
+            'fill-outline-color': data.properties.COLOR,
+          },
+        });
+    });
+  }, [map, paths]);
+
   return (
     <div>
       <div className="sidebar">
