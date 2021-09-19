a53cfbd55251a775014a9730863b5fc8dac4f3e9
Rithvik <rbhogavilli@gmail.com>
2021-09-19 03:36:35 -0500

    app context hooks

--- frontend/pinnacle-2021/src/App.js
+++ frontend/pinnacle-2021/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import {
   BrowserRouter as Router,
@@ -10,8 +10,19 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './components/Map/styles.css';
 
 import Map from './components/Map';
+import AppContext from "./context/AppContext";
 
 function App() {
+  const [paths, setPaths] = useState([])
+   const [timer, setTimer] = useState(false)
+
+  const [time, setTime] = useState(-1)
+  const [timeScalar, setTimeScalar] = useState(1)
+  const [startTime, setStartTime] = useState(-1)
+  const [endTime, setEndTime] = useState(-1)
+
+  const [map, setMap] = useState(null)
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
