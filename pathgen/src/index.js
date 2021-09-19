12cf3ea2694c9080f0876534c5b2995f0e11c817
Rithvik <rbhogavilli@gmail.com>
2021-09-19 01:25:22 -0500

    Get paths

--- pathgen/src/index.js
+++ pathgen/src/index.js
@@ -1,12 +1,16 @@
 const dotenv = require("dotenv");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
-dotenv.config()
+dotenv.config();
 
-console.log(process.env.MAPBOX_ACCESS_TOKEN)
+console.log(process.env.MAPBOX_ACCESS_TOKEN);
 
 
 var base = "https://api.mapbox.com/directions/v5/mapbox/cycling/";
+
+var schedules = JSON.parse(fs.readFileSync(path.join(__dirname, "schedules.json")))
 
 async function getRoute(waypoints) {
     points = ""
@@ -20,6 +24,8 @@ async function getRoute(waypoints) {
 
     const req = await axios.get(`${base}${points}?geometries=geojson&access_token=${process.env.MAPBOX_ACCESS_TOKEN}`)
 
+    console.log(req)
+
     return {
         "type": "Feature",
 	    "geometry": {
@@ -31,6 +37,37 @@ async function getRoute(waypoints) {
 }
 
 
-getRoute([[-84.518641, 39.134270], [-84.512023, 39.102779]]).then((res) => {
-    console.log(res)
+let routes = []
+async function getAll() {
+    for(let i = 0; i <schedules.length;i++) {
+        let schedule = schedules[i];
+let route = []
+        for (day in schedule) {
+            schedule[day].forEach(cls => {
+                console.log(cls)
+                route.push([cls.coords[1], cls.coords[0]])
+            })
+        }
+
+        let pth = await getRoute(route)
+
+        console.log(pth)
+        routes.push({
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": pth
+            }
+        })
+
+    }
+
+}
+
+getAll().then(res => {
+    fs.writeFileSync(path.join(__dirname, "paths.json"), JSON.stringify(routes))
 })
+
+// getRoute([[-84.518641, 39.134270], [-84.512023, 39.102779]]).then((res) => {
+//     console.log(res)
+// })
